df38bcf43b43ebb7f6ce04d86571fd585f6046cd
Emmanuel Lucky <emmanueltech2019@gmail.com>
2024-12-13 16:11:47 +0100

    updates

--- src/app/_components/Home.tsx
+++ src/app/_components/Home.tsx
@@ -92,13 +92,13 @@ const Home = () => {
                 
             </section>
 
-            <motion.div
+            {/* <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 1 }}
                 className="dashedArrow absolute -left-0 bottom-0 w-[128.68px] lg:w-[433.21px] -translate-x-1/3 md:-translate-x-0 lg:translate-x-[60%] -translate-y-full lg:-translate-y-1/2">
                 <Image src={dashedArrow} alt="dashed arrow" className={`object-cover`} />
-            </motion.div>
+            </motion.div> */}
         </div>
     )
 }
